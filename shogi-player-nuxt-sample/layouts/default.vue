bc294dc382e56eff431335e59a012e8b0c7e4d75
souppower <r.kubota92@gmail.com>
2022-12-06 02:00:48 +0900

    nuxt実装

---
+++ shogi-player-nuxt-sample/layouts/default.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+div
+  nav.navbar.header.is-primary
+    .navbar-brand
+      a.navbar-item(href="/")
+        img(src="~assets/buefy.png" height="28")
+  Nuxt
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>

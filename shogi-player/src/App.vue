8d02074c9addd1ec9bcdbaeccd382806d99c8278
souppower <r.kubota92@gmail.com>
2022-12-05 15:30:17 +0000

    クラス名rename

--- shogi-player/src/App.vue
+++ shogi-player/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="shogi-player">
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style>
-#app {
+#shogi-player {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;

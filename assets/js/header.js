20f267bea7ce6423c2a34642bb83ed3a701352f3
kima <aramaki.yusuke601@mail.kyutech.jp>
2023-04-01 21:59:49 +0900

    ヘッダーメニューにカーソルを合わせたら光るように変更

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,15 +1,17 @@
-let active;
-let test ;
+let menu_id;
 
 document.addEventListener("mouseover", function(event) {
-  active = $(event.target).attr("class");
-  console.log(active);
+  menu_id = $(event.target).attr("id");
+  console.log(menu_id);
+  if (menu_id == "menu1" || menu_id == "menu2" || menu_id == "menu3" || menu_id == "menu4"
+      || menu_id == "menu1_sub") {
+    document.getElementById(menu_id).classList.toggle("single_text");
+  }
 })
 
-
-
-
-
-console.log(test);
-console.log(active);
-
+document.addEventListener("mouseout", function(event) {
+  menu_id = $(event.target).attr("id");
+  if (menu_id == "menu1" || menu_id == "menu2" || menu_id == "menu3" || menu_id == "menu4") {
+    document.getElementById(menu_id).classList.toggle("single_text");
+  }
+})

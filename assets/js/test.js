9c426e4f56300fe0faa986acc892910fe121e9d9
kima <aramaki.yusuke601@mail.kyutech.jp>
2023-04-28 22:08:09 +0900

    header.cssとfooter.css作成、盤面の色を変える関数作成

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,12 +1,21 @@
-let elem = document.getElementById("shogi");
-
-
 //引数に手番、位置(２六など)、駒の種類、移動する前の位置(25など)を渡してそのposに駒を移動させる関数
 //注意：oldPosは整数型で渡せるが、それ以外の引数はstring型で渡す。
 //　　　また、tebannは半角、posとkomaに関しては全角で渡す。
 //　　　例：SetKoma('1', '２八', '歩', 27)
-//　　　この関数はsource属性を入れ替えるものなので既存の棋譜は消えてしまう->改良すべき
+//　　　この関数はsource属性を上書きするものなので既存の棋譜は消えてしまう->改良すべき
+
+
 function SetKoma(tebann, pos, koma, oldPos) {
-  elem.setAttribute("source", tebann + " " + pos + koma + "(" + oldPos + ")");
-  elem.setAttribute("turn", tebann);
+  var elem = document.getElementById("shogi");  //idからHTMLElementを取得
+
+  elem.setAttribute("source", "\n" + tebann + " " + pos + koma + "(" + oldPos + ")");  //source属性を上書き
+  elem.setAttribute("turn", tebann);  //その手番に移動
 }
+
+function EmphasizeSquare(rows, cols) {
+  var elem = document.getElementById("shogi");  //idからHTMLElementを取得
+
+  elem.setAttribute("pass_css", ".place_" + rows + "_" + cols + " { background-color: LightPink; }");
+
+}
+

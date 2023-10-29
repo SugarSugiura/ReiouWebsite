6194329a16897a921f5fc8829272873f2d572968
YusukeLuxaid <87661726+YusukeLuxaid@users.noreply.github.com>
2023-10-29 18:15:37 +0900

    ヘッダーをコンポーネント化

--- assets/js/reiou-header.js
+++ assets/js/reiou-header.js
@@ -1,9 +1,403 @@
 import { LitElement, css, html } from "https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js"
 export class ReiouHeader extends LitElement {
 	static styles = css`
-    header {
-      
+  :host {
+    --main-color: #004097;
+    --sub-color:  #5E8DC3;
+    --transition-color: 0.2s;
+
+    width: 100%;
+    height: 60px;
+    display: block;
+    position: fixed;
+  }
+
+  html, body, div, span, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  abbr, address, cite, code,
+  del, dfn, em, img, ins, kbd, q, samp,
+  small, strong, sub, sup, var,
+  b, i,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section, summary,
+  time, mark, audio, video {
+    margin:0;
+    padding:0;
+    border:0;
+    outline:0;
+    font-size:100%;
+    vertical-align:baseline;
+    background:transparent;
+  }
+  ul {
+    list-style-type: none;
+  }
+  li {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+  }
+  
+  * {
+    box-sizing: border-box;
+    border-style: solid;
+    border-width: 0px;
+  }
+  
+  header {
+    width: 100%;
+    height: 60px;
+    background-color: #fff;
+    position: fixed;
+    z-index: 10;
+    box-shadow: rgb(230, 230, 230) 0px 2px 6px;
+  }
+  
+  nav {
+    background-color: #121212;
+  }
+  
+  .header_container {
+  display: flex;
+  flex-direction:row;
+  height:100%;
+  }
+  
+  header .site-title {
+  padding: 10px 1vw;
+  /*header自体の位置(間隔)*/
+  flex-basis:10%;
+  text-align: center;
+  }
+  
+  header .site-title a {
+    display: block;
+    height: 100%;
+  }
+  
+  #navi {
+    background-color: var(--main-color);
+  }
+  
+  #navi a {
+    color: #fff;
+  }
+  
+  .menu {
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+  padding: 0;
+  }
+  
+  .menu > li {
+  width: 25%;
+  height: 100%;
+  line-height: 400%;
+  text-align: center;
+  }
+  
+  .menu > li a {
+    display: block;
+  }
+  
+  .single:hover::before {
+    width: 100%;
+  }
+  
+  .single::before {
+    content: "";
+    width: 0;
+    height: 3px;
+    background-color: #fff;
+    position: absolute;
+    bottom: 0px;
+    left: 50%;
+    transform: translateX(-50%);
+    border-radius: 3px;
+    transition: width .3s ease;
+  }
+  
+  .menu > .single {
+    position: relative;
+  }
+  
+  .menu-second {
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0.5s, visibility 0.5s;
+    border-radius: 0.5rem;
+    /* border-left: 4px solid transparent; */
+  }
+  
+  .menu-second li:hover {
+    background-color: var(--sub-color);
+    border-radius: 0.5rem;
+    
+  }
+  
+  
+  .single .menu-second {
+    width: 100%;
+    background-color: #444;
+    position: relative;
+    top: 0px;
+    left: 0;
+  }
+  .single .menu-second li a:hover {
+    /* opacity: 0.7; */
+    transition: opacity 0.5s;
+    border-left: solid;
+  }
+  
+  .single:hover .menu-second {
+    visibility: visible;
+    opacity: 1;
+  }
+  
+  #menu2 {
+    border-left: 1px solid ;
+  }
+  
+  #menu3 {
+    border-left:1px solid;
+    border-right:1px solid;
+  }
+  
+  .pc-area {
+    display: block;
+    /* pc版を表示 */
+    flex-basis: 80%;
+  }
+  .phone-area {
+    display: none;
+    /* スマホ版を非表示 */
+  }
+  .toggle_btn {
+    right: 20px;
+    display: none;
+    /* スマホ版を非表示 */
+  }
+  
+  #navi li {
+    font-size: 15px;
+    /* padding: 10px 60px 10px 0; */
+    margin: auto;
+    letter-spacing: 0.1em;
+    font-weight: 550;
+    /*ヘッダー枠内のmenuの位置*/
+    /*margin-right: 0px;*/
+    /*ヘッダー枠内のmenuの間隔*/
+  }
+  
+  @media screen and (max-width: 767px) {
+    .pc-area {
+      display: none;
     }
+
+    .phone-area {
+      display: block;
+    }
+
+    header .site-title {
+      padding: auto;
+    }
+
+    header {
+      width: 100%;
+      height: 60px;
+      background-color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      position: fixed;
+      left: 0;
+      right: 0;
+      z-index: 10;
+    }
+  
+    .header_container {
+      display: block;
+      left: 15px;
+      position: relative;
+    }
+  
+  
+  
+    .test {
+      visibility: hidden;
+    }
+  
+    #navi-phone {
+      display: block;
+      position: fixed;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      left: -1000px;
+      padding-top: 60px;
+      z-index: 100;
+      transition: opacity 0.3s;
+      background: #293949;
+      padding: 80px 8vw 8vw 8vw;
+      opacity: 0;
+      z-index: 0;
+    }
+  
+    .phone-menu {
+      width: 100%;
+      max-width: 360px;
+      margin: 30px auto 20px;
+      background: #FFF;
+      /* -webkit-border-radius: 4px;
+      -moz-border-radius: 4px;
+      border-radius: 4px; */
+    }
+   
+   .phone-menu .link {
+     cursor: pointer;
+     display: block;
+     padding: 15px 15px 15px 42px;
+     color: #fff;
+     background-color: #293949;
+     font-size: 14px;
+     font-weight: 700;
+     border-bottom: 1px solid #CCC;
+     position: relative;
+     -webkit-transition: all 0.4s ease;
+     -o-transition: all 0.4s ease;
+     transition: all 0.4s ease;
+   }
+   
+   .phone-menu li:last-child .link {
+     border-bottom: 0;
+   }
+   
+   .phone-menu li i {
+     position: absolute;
+     top: 16px;
+     right: 12px;
+     font-size: 18px;
+     color: #fff;
+     -webkit-transition: all 0.4s ease;
+     -o-transition: all 0.4s ease;
+     transition: all 0.4s ease;
+   }
+  
+   .phone-menu li i:before {
+    content: "\f067";
+   }
+   
+   .phone-menu li.open .link {
+     color: #4aa5f0;
+   }
+   
+   .phone-menu li.open i {
+     color: #4aa5f0;
+   }
+   .phone-menu li.open i:before {
+     content: "\f068";
+   }
+   
+   .phone-menu li.default .phone-submenu {display: block;}
+
+    .phone-submenu {
+      display: none;
+      background: #23313f;
+      font-size: 14px;
+    }
+   
+    .phone-submenu li {
+      border-bottom: 1px solid #4b4a5e;
+    }
+   
+    .phone-submenu a {
+      display: block;
+      text-decoration: none;
+      color: #d9d9d9;
+      padding: 12px;
+      padding-left: 42px;
+      -webkit-transition: all 0.25s ease;
+      -o-transition: all 0.25s ease;
+      transition: all 0.25s ease;
+    }
+   
+    .phone-submenu a:hover {
+      background: #b63b4d;
+      color: #FFF;
+    }
+
+    .open #navi-phone {
+      left: 0;
+      opacity: 1;
+      z-index: 20;
+    }
+    .toggle_btn {
+      display: block;
+      width: 30px;
+      height: 30px;
+      position: relative;
+      transition: all 0.5s;
+      cursor: pointer;
+      z-index: 101;
+    }
+
+    .toggle_btn span {
+      display: block;
+      position: absolute;
+      width: 30px;
+      height: 2px;
+      background-color: #333;
+      border-radius: 4px;
+      transition: all 0.5s;
+    }
+
+    .open .toggle_btn span:nth-child(1) {
+      top: 7px;
+    }
+  
+    .toggle_btn span:nth-child(1) {
+      top: 6px;
+    }
+
+    .toggle_btn span:nth-child(2) {
+      bottom: 14px;
+    }
+  
+    .toggle_btn span:nth-child(3) {
+      bottom: 6px;
+    }
+  
+    .open .toggle_btn span {
+      background-color: #fff;
+    }
+
+    .open .toggle_btn span:nth-child(1) {
+      -webkit-transform: translateY(4px) rotate(-45deg);
+      transform: translateY(8px) rotate(-45deg);
+    }
+
+    .open .toggle_btn span:nth-child(2) { 
+    opacity: 0;
+    } 
+
+    .open .toggle_btn span:nth-child(3) {
+      -webkit-transform: translateY(-4px) rotate(45deg);
+      transform: translateY(-7px) rotate(45deg);
+    }
+  
+    #mask {
+      display: none;
+      transition: all 0.5s;
+    }
+  }
 	`
 	static properties = {
 
@@ -16,14 +410,14 @@ export class ReiouHeader extends LitElement {
 
 	render() {
     return html`
-    <header>
+    <header id="header">
       <div></div>
       <div class="header_container">
         <div class="site-title">
-          <a href="index.html"><img src="assets/img/REIOUlogo.png" alt="REIOU"></a>
+          <a href="index.html"><img src="../../img/REIOUlogo.png" alt="REIOU"></a>
         </div>
-        <nav id="navi" class="pc-area"><!--元は-->
-          <ul class="menu active"><!--元はmenu-->
+        <nav id="navi" class="pc-area">
+          <ul class="menu active">
             <li class="single">
               <a href="index.html" id="menu1" class="single_text">学習</a>
               <ul class="menu-second">
@@ -60,8 +454,105 @@ export class ReiouHeader extends LitElement {
           </h1>
         </div>
       </div>
+
+      <nav id="navi-phone" class="phone-area">
+          <ul id="phone-menu" class="phone-menu">
+            <li>
+              <div class="link">
+                学習<i class="fa"></i>
+              </div>
+              <ul class="phone-submenu">
+                  <li><a href="index.html">戦法定跡</a></li>
+                  <li><a href="quiz.html">次の一手</a></li>
+                  <li><a href="tsume-shogi.html">詰将棋</a></li>
+                  <li><a href="assets/html/slide.html">YouTube動画</a></li>
+              </ul>
+            </li>
+            <li>
+              <div class="link">
+                記事<i class="fa"></i>
+              </div>
+              <ul class="phone-submenu">
+                <li><a href="joseki-slide.html">棋戦情報</a></li>
+                <li><a href="quiz.html">プロの棋譜解説</a></li>
+                <li><a href="assets/html/video.html">動画ページ</a></li>
+              </ul>
+            </li>
+            <li>
+              <div class="link">
+                紹介<i class="fa"></i>
+              </div>
+            </li>
+            <li>
+              <div class="link">
+                プラン<i class="fa"></i>
+              </div>
+            </li>
+            <!-- <li><a href="#">お問い合わせ</a></li> -->
+          </ul>
+        </nav>
+        
+        <div class="toggle_btn" @click="${this.toggle_phone_header}">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div id="mask" @click="${this.close_phone_header}"></div>
     </header>
 		`
 	}
+
+  toggle_phone_header() {
+    const elem = this.shadowRoot.getElementById("header");
+    console.log(elem);
+    if (elem.classList[0] == "open") {
+      elem.classList.remove("open");
+    } else {
+      elem.classList.add("open");
+    }
+  }
+
+  close_phone_header() {
+    const elem = this.shadowRoot.getElementById("header");
+    elem.classList.remove("open");
+  }
+
+// $(function(){
+//     /*=================================================
+//     ハンバーガーメニュー
+//     ===================================================*/
+//     // ハンバーガーメニューのクリックイベント
+//     $('.toggle_btn').on('click', function() {
+//       // #headerにopenクラスが存在する場合
+//       if ($('#header').hasClass('open')) {
+//         // openクラスを削除
+//         // openクラスを削除すると、openクラスのCSSがはずれるため、
+//         // メニューが非表示になる
+//         $('#header').removeClass('open');
+  
+//       // #headerにopenクラスが存在しない場合
+//       } else {
+//         // openクラスを追加
+//         // openクラスを追加すると、openクラスのCSSが適応されるため、
+//         // メニューが表示される
+//         $('#header').addClass('open');
+//       }
+//     });
+
+//     $('.toggle_btn').on('click', function() {
+//       if ($('#shogi-player').hasClass('sp')) {
+//         $('#shogi-player').removeClass('sp');
+//       } else {
+//         $('#shogi-player').addClass('sp');
+//       }
+//     }); 
+  
+//     // メニューが表示されている時に画面をクリックした場合
+//     $('#mask').on('click', function() {
+//       // openクラスを削除して、メニューを閉じる
+//       $('#header').removeClass('open');
+//     });
+//   });
+
 }
 customElements.define("reiou-header", ReiouHeader);

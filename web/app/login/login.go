579ed5e4c249b7f13420f809272b194139b8a95e
YusukeLuxaid <87661726+YusukeLuxaid@users.noreply.github.com>
2024-05-01 17:44:18 +0900

    ランダム文字列とセッションの固有値を結びつけ

--- web/app/login/login.go
+++ web/app/login/login.go
@@ -5,16 +5,19 @@ package login
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"log"
 	"net/http"
 
 	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 
 	"github.com/SugarSugiura/ReiouWebsite/platform/authenticator"
 )
 
 // Handler for our login.
-func Handler(auth *authenticator.Authenticator) gin.HandlerFunc {
+func Handler(auth *authenticator.Authenticator, store cookie.Store) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		state, err := generateRandomState()
 		if err != nil {
@@ -29,6 +32,37 @@ func Handler(auth *authenticator.Authenticator) gin.HandlerFunc {
 			ctx.String(http.StatusInternalServerError, err.Error())
 			return
 		}
+
+		//ここからredis
+		profile := session.Get("profile")
+
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     "redis:6379", // Redisのアドレスとポート
+			Password: "",           // Redisに設定したパスワード（設定していない場合は空文字列）
+			DB:       0,            // 使用するデータベース番号
+		})
+
+		stateVal := session.Get("state")
+		stateStr, ok := stateVal.(string)
+		if !ok {
+			// state が文字列でない場合のエラーハンドリング
+			return
+		}
+
+		err = rdb.Set(ctx, stateStr, profile.(map[string]interface{})["aud"].(string), 0).Err()
+		if err != nil {
+			log.Println(err)
+		}
+
+		log.Println("セット終わり")
+
+		val, err := rdb.Get(ctx, stateStr).Result()
+		if err != nil {
+			panic(err)
+		}
+		log.Println(stateStr, val)
+
+		log.Println("ゲット終わり")
 
 		ctx.Redirect(http.StatusTemporaryRedirect, auth.AuthCodeURL(state))
 	}

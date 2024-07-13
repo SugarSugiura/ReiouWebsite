9cb9130b099a73281112262ebc6cee4fdcf7c76a
souppower <r.kubota92@gmail.com>
2024-07-13 22:19:25 +0900

    セッションのTTLを1時間にした

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	sql "database/sql"
 	"encoding/gob"
-	"fmt"
 	"github.com/SugarSugiura/ReiouWebsite/platform/middleware"
 	"github.com/gin-contrib/sessions/redis"
 	"log"
@@ -48,14 +47,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("DB successfully connected!")
-
-	//redisClient := redis.NewClient(&redis.Options{
-	//	Addr:     "redis:6379", // Redisのアドレスとポート
-	//	Password: "",           // Redisに設定したパスワード（設定していない場合は空文字列）
-	//	DB:       0,            // 使用するデータベース番号
-	//})
-
 	rtr := newRouter(auth, db)
 
 	log.Print("Server listening on http://localhost:8000/")
@@ -65,19 +56,18 @@ func main() {
 }
 
 // NewRouter registers the routes and returns the router.
-// ここでは関数名(引数 引数の型) 関数の返り値の型
 func newRouter(auth *authenticator.Authenticator, db *sql.DB) *gin.Engine {
 	router := gin.Default()
 
-	// To store custom types in our cookies,
-	// we must first register them using gob.Register
 	gob.Register(map[string]interface{}{})
 
-	// Redisセッションストアの設定
 	redisStore, err := redis.NewStore(10, "tcp", "redis:6379", "", []byte("secret"))
 	if err != nil {
 		log.Fatalf("Failed to create redis store: %v", err)
 	}
+	redisStore.Options(sessions.Options{
+		MaxAge: 3600,
+	})
 	router.Use(sessions.Sessions("auth-session", redisStore))
 
 	router.Static("/page", "./assets/html")
@@ -97,8 +87,11 @@ func newRouter(auth *authenticator.Authenticator, db *sql.DB) *gin.Engine {
 	userService := user.NewUserService(db)
 	router.GET("/login", login.Handler(auth))
 	router.GET("/callback", callback.Handler(auth, userService))
-	router.GET("/user", middleware.IsAuthenticated, user.Handler(userService))
-	router.GET("/logout", logout.Handler)
+
+	authRequired := router.Group("/")
+	authRequired.Use(middleware.IsAuthenticated)
+	authRequired.GET("/user", user.Handler(userService))
+	authRequired.GET("/logout", logout.Handler)
 
 	return router
 }
